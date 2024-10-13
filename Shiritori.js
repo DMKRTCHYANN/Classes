8193f2dc656ad9b3b5cb7eecc637fa5c966cde80
DMKRTCHYANN <mkrtchyandavv@gmail.com>
2024-10-13 23:10:10 +0400

    CostomConsole

--- Shiritori.js
+++ Shiritori.js
@@ -1,48 +1,64 @@
-class Shiritory {
-    #words;
-    #gameOver;
-
-    constructor() {
-        this.#words = [];
-        this.#gameOver = false;
+class Console {
+    constructor(type) {
+        this.type = type;
+        this._history = [];
     }
 
-    play(word) {
-        if (this.#gameOver) {
-            return "Game is already over. Please restart to play again.";
-        }
+    get history() {
+        return this._history;
+    }
 
-        if (this.#words.length === 0) {
-            this.#words.push(word);
-            return this.#words;
+    set history(a) {
+        // No-op, we're not allowing external modification of _history directly
+    }
+
+    log(...args) {
+        if (args.length === 1 && this.type === "Regular") {
+            this._history.push(`${this.type} : [ ${JSON.stringify(args[0])} ]`);
+            console.log(`${this.type} : [ ${JSON.stringify(args[0])} ]`);
+        } else if (args.length === 1 && this.type === "Fancy") {
+            this._history.push(`${this.type} : ` + JSON.stringify(args[0], null, 2));
+            console.log(`${this.type} : ` + JSON.stringify(args[0], null, 2));
         } else {
-            const lastWord = this.#words[this.#words.length - 1];
-            const lastChar = lastWord[lastWord.length - 1];
+            let res = "";
+            for (let i = 1; i < args.length; i++) {
+                if (i !== args.length - 1) res += args[i] + ", ";
+                else res += args[i];
+            }
+            this._history.push(`${args[0]} ${res}`);
+            console.log(`${args[0]} ${res}`);
+        }
+    }
 
-            if (lastChar === word[0] && !this.#words.includes(word)) {
-                this.#words.push(word);
-                return this.#words;
-            } else if (lastChar !== word[0]) {
-                this.#gameOver = true;
-                return `Game over - ${word} should start with "${lastChar}"`;
-            } else {
-                this.#gameOver = true;
-                return `Game over - ${word} has already been said`;
+    history(range = [0, this._history.length]) {
+        const [start, end] = range;
+        let res = "";
+
+        // Ensure range is within bounds
+        const actualEnd = Math.min(end, this._history.length);
+
+        for (let i = start; i < actualEnd; i++) {
+            if (this._history[i]) {
+                res += this._history[i] + "\n";
             }
         }
+        console.log(res);
     }
 
-    restart() {
-        this.#words = [];
-        this.#gameOver = false;
-        return "Game restarted";
+    clearHistory() {
+        this._history.length = 0;
+        console.log(true);
     }
 }
 
-const myShiritori = new Shiritory();
+const myConsole = new Console('Regular');
+const fancyConsole = new Console('Fancy');
 
-console.log(myShiritori.play("apple")); 
-console.log(myShiritori.play("ear")); 
-console.log(myShiritori.play("rhino"));
-console.log(myShiritori.play('begemot'));
+// Test cases
+myConsole.log([0, 1, 2, 3]); // "Regular: [0,1,2,3]"
+fancyConsole.log({ a: 1, b: 2 }); // "Fancy: {a: 1, b: 2}"
+myConsole.log("ok : ", 1, 2, 3); // "ok : 1, 2, 3"
+myConsole.clearHistory(); // true
+myConsole.history(); // ""
+
 
